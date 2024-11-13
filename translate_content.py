cf0475f83b54234d0b1b15082d98974e8359e106
Ebru Genç <gencebru17@gmail.com>
2024-11-13 14:09:55 +0300

    modify

--- translate_content.py
+++ translate_content.py
@@ -1,14 +1,12 @@
-from google_trans_new import google_translator
+from googletrans import Translator
 import os
 
 def combine_text_files(input_path, combined_file_path):
-    # Tüm sayfa txt dosyalarını sırayla tek bir dosyada birleştir
     with open(combined_file_path, "w", encoding="utf-8") as combined_file:
         for filename in sorted(os.listdir(input_path), key=lambda x: int(x.split('.')[0])):
             if filename.endswith(".txt"):
                 file_path = os.path.join(input_path, filename)
 
-                # Her sayfadaki Korece metni oku ve tek dosyaya ekle
                 with open(file_path, "r", encoding="utf-8") as f:
                     korean_text = f.read()
                     combined_file.write(korean_text + "\\n\\n")
@@ -16,16 +14,16 @@ def combine_text_files(input_path, combined_file_path):
     print(f"Korece tüm metinler {combined_file_path} dosyasına birleştirildi.")
 
 def translate_file(input_file_path, output_file_path, src_lang, dest_lang):
-    translator = google_translator()
+    translator = Translator()
+    translator = Translator(service_urls=['translate.googleapis.com'])
 
     # Giriş dosyasındaki metni oku
     with open(input_file_path, "r", encoding="utf-8") as f:
         text = f.read()
 
     # Metni belirtilen dile çevir
-    translated_text = translator.translate(text, lang_src=src_lang, lang_tgt=dest_lang)
+    translated_text = translator.translate(text, src=src_lang, dest=dest_lang).text
 
-    # Çeviriyi çıktı dosyasına yaz
     with open(output_file_path, "w", encoding="utf-8") as f:
         f.write(translated_text)
 
@@ -33,7 +31,7 @@ def translate_file(input_file_path, output_file_path, src_lang, dest_lang):
 
 # Kullanım
 input_path = "output/output_text/"              # Sıralı Korece metinlerin olduğu klasör
-combined_file_path = "output/korean.txt"        # Birleştirilen Korece dosyası
+combined_file_path = "output/output_translated/korean.txt"        # Birleştirilen Korece dosyası
 english_output_path = "output/translated_eng.txt"  # İngilizce çeviri dosyası
 turkish_output_path = "output/translated_tur.txt"  # Türkçe çeviri dosyası
 
