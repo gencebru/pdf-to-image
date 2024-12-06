ec2f173664150624a5340774a8c5be53b6de9a78
Ebru Genç <gencebru17@gmail.com>
2024-12-06 09:22:04 +0300

    add pdf conv for set

---
+++ pdf-converter-set.py
@@ -0,0 +1,31 @@
+from pdf2image import convert_from_path
+import os
+
+
+def pdf_to_images(input_dir, output_dir):
+    if not os.path.exists(input_dir):
+        raise FileNotFoundError(f"Girdi klasörü bulunamadı: {input_dir}")
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    for pdf_file in os.listdir(input_dir):
+        if pdf_file.endswith(".pdf"):
+            pdf_path = os.path.join(input_dir, pdf_file)
+            pdf_name = os.path.splitext(pdf_file)[0]
+            pdf_output_dir = os.path.join(output_dir, pdf_name)
+
+            if not os.path.exists(pdf_output_dir):
+                os.makedirs(pdf_output_dir)
+
+            pages = convert_from_path(pdf_path)
+            for page_number, page in enumerate(pages, start=1):
+                output_filename = os.path.join(pdf_output_dir, f"{page_number}.jpg")
+                page.save(output_filename, "JPEG")
+                print(f"{output_filename} kaydedildi.")
+
+
+input_dir = "input/scanned-pdf"
+output_dir = "/Users/ebrugenc/develop/Github/pdf-to-image/output/pdf"
+
+pdf_to_images(input_dir, output_dir)
